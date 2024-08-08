e77d780309afce957475369f10b7abd80bb5872c
Ari Oppenheimer <oppenheimera@users.noreply.github.com>
2024-08-08 15:01:58 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ari Oppenheimer on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
