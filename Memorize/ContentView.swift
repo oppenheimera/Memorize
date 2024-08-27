5bb8eee8699b463cf6c92518b615854cfc00572e
Ari Oppenheimer <oppenheimera@users.noreply.github.com>
2024-08-27 15:25:01 -0700

    Assignment 01

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,34 +7,78 @@
 
 import SwiftUI
 
+let halloween: [String] = ["👻", "🎃", "🧙", "🏚️", "😈", "🕷️", "💀", "🥧", "🕸️", "👻", "🎃", "🧙", "🏚️", "😈", "🕷️", "💀", "🥧", "🕸️"]
+let beach: [String] = ["🌊","👙", "🏖️", "☀️", "🐚", "⛱️", "🏝️", "🩱", "🩳", "🌊","👙", "🏖️", "☀️", "🐚", "⛱️", "🏝️", "🩱", "🩳"]
+let vehicles: [String] = ["🚗", "🚙", "🏎️", "🚃", "🚖", "🚔", "🚢", "🚎", "🚍", "🚗", "🚙", "🏎️", "🚃", "🚖", "🚔", "🚢", "🚎", "🚍"]
+let food: [String] = ["🌮", "🌯", "🌭", "🥗", "🍕", "🧁", "🍦", "🥑", "🥐", "🌶️", "🌮", "🌯", "🌭", "🥗", "🍕", "🧁", "🍦", "🥑", "🥐", "🌶️"]
+
 struct ContentView: View {
-    var body: some View {
-      HStack{
-        CardView(isFaceUp: true)
-        CardView()
-        CardView()
-        CardView()
+  @State var emojis = halloween
+  
+  var body: some View {
+    VStack{
+      Text("Memorize!").bold().font(.system(size: 32))
+      ScrollView {
+        drawCards()
       }
-        .foregroundColor(.orange)
-        .padding()
-        
+      Spacer()
+      themeAdjusters
     }
+    .padding()
+  }
+  
+  func drawCards() -> some View {
+    emojis.shuffle()
+    return LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]){
+      ForEach(0..<emojis.count, id: \.self) { index in
+        CardView(content: emojis[index])
+          .aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+      }
+    }.foregroundColor(.orange)
+  }
+
+  var themeAdjusters: some View {
+    HStack{
+      themeChooser(emoji: beach, themeName: "Beach", symbol: "beach.umbrella")
+      Spacer()
+      themeChooser(emoji: vehicles,themeName: "Vehicles", symbol: "car")
+      Spacer()
+      themeChooser(emoji: food, themeName: "Food", symbol: "fork.knife")
+      Spacer()
+      themeChooser(emoji: halloween, themeName: "Halloween", symbol: "moon")
+    }
+  }
+
+  func themeChooser(emoji: [String], themeName: String, symbol: String) -> some View {
+    Button(action: {
+      emojis = emoji
+      drawCards()
+    }, label: {
+      VStack {
+        Image(systemName: symbol).imageScale(.large)
+        Text(themeName)
+      }.foregroundColor(.orange)
+    })
+  }
 }
 
 struct CardView: View {
-  var isFaceUp: Bool = false
+  let content: String
+  @State var isFaceUp: Bool = false
   
   var body: some View {
     ZStack {
-      if isFaceUp {
-        RoundedRectangle(cornerRadius: 12)
-          .foregroundColor(.white)
-        RoundedRectangle(cornerRadius: 12)
-          .strokeBorder(lineWidth: 2)
-        Text("👻").font(.largeTitle)
-      } else {
-       RoundedRectangle(cornerRadius: 12)
+      let base = RoundedRectangle(cornerRadius: 12)
+      Group {
+        base.fill(.white)
+        base.strokeBorder(lineWidth: 2)
+        Text(content).font(.largeTitle)
       }
+      .opacity(isFaceUp ? 1 : 0)
+      base.fill().opacity(isFaceUp ? 0 : 1)
+    }
+    .onTapGesture {
+      isFaceUp.toggle()
     }
   }
 }
